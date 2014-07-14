86defa1ac0c07d7e83b40ec390dd8e101e5455d4
Pan Blbej Tuňák <bart@bartweb.cz>
2014-07-14 13:08:57 +0200

    gateway and htaccess generator (experimental)
    Experimental and hardcoded

--- skeletonx/setContextBabel.snippet.php
+++ skeletonx/setContextBabel.snippet.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /**
  * setContextBabel (experimental)
  *
@@ -51,5 +51,77 @@
     //  Clear context cache //TODO
         $cacheRefreshOptions =  array( 'context_settings' => array('contexts' => array($contexts) ));
         $modx->cacheManager-> refresh($cacheRefreshOptions);
-        
-    return "[setContextBabel]: Babel settings created for all contexts. Now you should remove snippet call." . var_dump($contexts);
+    
+    // Generate gateway plugin (hardcoded instructions)
+    
+    $gateway = '<h2>Step #3 - Creating a "gateway" plugin</h2>
+                We will need to create a basic plugin to assign the correct context.<br>
+                In the MODX Manager go to <strong>Elements</strong> and click on the <strong>New Plugin</strong> icon.<br>
+                Name the plugin <strong>gateway</strong> and add the following Plugin code:<br>
+                ';
+    $gateway .= '<textarea rows="25" cols="150">';
+    $gateway .= (htmlentities('
+                <?php 
+                /* SkeletonX generated gateway plugin for babel: https://github.com/bartholomej/SkeletonX-for-MODX */
+                    if($modx->context->get(\'key\') != "mgr"){ 
+                    switch($_REQUEST[\'cultureKey\']) { 
+                        '));
+    
+    foreach ($contexts as $context) {
+        $gateway .= (htmlentities('
+                            case \''.$context.'\':
+                                $modx->switchContext(\''.$context.'\');
+                                break;   
+                    '));
+    }                
+    $gateway .= (htmlentities('
+                            default:
+                                /* Set the default context here */
+                                $modx->switchContext(\'web\');
+                                break;
+                    }
+                /* unset GET var to avoid
+                 * appending cultureKey=xy to URLs by other components */
+                unset($_GET[\'cultureKey\']);
+            }
+            '));
+    $gateway .= '</textarea>';
+    $gateway .= '<br>This gateway plugin will need to be activated by a OnHandleRequest, to do this go to the <strong>System Events</strong> tab, scroll down to the <strong>OnHandleRequest</strong> line and enable this.<br>
+                 Save the <strong>gateway</strong> plugin.';
+
+    // generate htaccess
+    
+    $htaccess = '<h2>Step #1 - Setting up .htaccess</h2>';
+    $htaccess .= '<textarea rows="25" cols="150">';
+    $htaccess .= '
+        # redirect all requests to /en/favicon.ico and /cs/favicon.ico
+        # to /favicon.ico
+        RewriteCond %{REQUEST_FILENAME} !-d
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteRule ^(cs|en)/favicon.ico$ favicon.ico [L,QSA]
+           
+        # redirect all requests to /en/assets* and /cs/assets* to /assets*
+        RewriteCond %{REQUEST_FILENAME} !-d
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteRule ^(cs|en)/assets(.*)$ assets$2 [L,QSA]
+          
+        # redirect all other requests to /en/* and /cs/*
+        # to index.php and set the cultureKey parameter
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteCond %{REQUEST_FILENAME} !-d
+        RewriteRule ^(cs|en)?/?(.*)$ index.php?cultureKey=$1&q=$2 [L,QSA]
+        </textarea><br>
+    ';
+    $ctxOutput = '<h2>Step #2 - Creating your contexts</h2>';
+    $ctxOutput .= '[setContextBabel]: Babel settings created for all contexts. Go to next step.<br/><code>' . print_r($contexts, true). '</code>';
+    
+    $instructions = '<h2>Step #4 - Installing Babel</h2>'; 
+    $instructions .= 'Now you should remove snippet call.<br/>
+                      Almost there! We now can install the Babel package from package manager.<br/>
+                      Based on this great manual: http://designfromwithin.com/blog-webdesign-development/2012/01/12/modx-multilingual-setting-up-babel-and-have-a-website-with-multible-languages/                  
+                      ';
+    
+    
+    
+    return $htaccess . $ctxOutput .  $gateway . $instructions;
+ 
