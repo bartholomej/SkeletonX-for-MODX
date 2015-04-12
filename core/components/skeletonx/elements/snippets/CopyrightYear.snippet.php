2027cc2f717de6d78802c9168656a781a73ad6c3
Pan Blbej Tuňák <bart@bartweb.cz>
2015-04-12 11:37:05 +0200

    Update getCopyrightYear snippet

--- core/components/skeletonx/elements/snippets/CopyrightYear.snippet.php
+++ core/components/skeletonx/elements/snippets/CopyrightYear.snippet.php
@@ -1,18 +1,17 @@
 <?php
 /**
- * CopyrightYear: get years to copyright footer
+ * getCopyrightYear: get years to copyright footer
  *
  * Parameters:
  *  start [optional]: First year of website.
- *      Special param: modx (Get modx installation date)
+ *      Special param: `modx` (Get modx installation date)
  *      DEFAULT: nothing.
  *  separator [optional]: the space string between start and current year
  *
- *
- * EXAMPLES: e.g. current year: 2014, MODx installed: 2013
- * [[CopyrightYear]]                // return: 2014
- * [[CopyrightYear? &start=`2010`]] // return: 2010 - 2014
- * [[CopyrightYear? &start=`modx`]] // return: 2013 - 2014
+ * EXAMPLES: e.g. current year: 2015, MODx installed: 2014
+ * [[getCopyrightYear]]                // return: 2015
+ * [[getCopyrightYear? &start=`2010`]] // return: 2010 - 2015
+ * [[getCopyrightYear? &start=`modx`]] // return: 2014 - 2015
  *
  * Author: BART!
  */
@@ -20,16 +19,16 @@
 $start = $modx->getOption('start', $scriptProperties, date("Y"));
 $separator = $modx->getOption('separator', $scriptProperties, ' - ');
 
-$now = date("Y");
-$start = isset($start)? $start : $now;
+$now = date('Y');
+$start = isset($start) ? $start : $now;
 
-if($start == 'modx'){ // Dirty, dirty way ho to get installation date :))
-    $setting = $modx->getObject('modSystemSetting', 'emailsender');
-        if($setting){
+if ($start == 'modx'){ // Dirty, dirty way ho to get installation date :))
+    $setting = $modx->getObject('modSystemSetting', 'welcome_screen');
+        if ($setting) {
             $settings = $setting->toArray();
             $start = date('Y', strtotime($settings['editedon']));
-        }else{
-            $modx->log(modX::LOG_LEVEL_ERROR, '[CopyrightYear] Cant get date from system settings property!');
+        } else {
+            $modx->log(modX::LOG_LEVEL_WARN, '[[getCopyrightYear]] Can\'t get date from system settings property!');
         }
 }
 
